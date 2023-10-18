99061943c0cfe78e136fb3c5d5d608a3c3f6a062
ktxdev <sean.ktxdev@gmail.com>
2023-10-18 21:27:16 +0200

    Destructure post details

--- frontend/src/features/posts/Post.jsx
+++ frontend/src/features/posts/Post.jsx
@@ -4,22 +4,28 @@ import { AiFillLike, AiOutlineLike } from "react-icons/ai";
 import { FaRegCommentDots } from "react-icons/fa6";
 import Button from "../../ui/Button";
 
-function Post({ post }) {
+function Post({
+  post: {
+    text,
+    imageUrl,
+    postedBy: { firstName, lastName, profileImageUrl },
+  },
+}) {
   return (
     <li>
       <Card className="space-y-2 py-2">
         <div className="flex items-center px-4 gap-1">
-          <Avatar size={16} avatar={post.postedBy.profileImageUrl} />
+          <Avatar size={16} avatar={profileImageUrl} />
           <div>
             <h3 className="text-sm font-semibold">
-              {post.postedBy.firstName} {post.postedBy.lastName}
+              {firstName} {lastName}
             </h3>
             <p className="text-sm">Java Backend Developer</p>
           </div>
         </div>
         <div>
-          <p className="text-sm px-4 py-2">{post.text}</p>
-          {post.imageUrl && <img src={post.imageUrl} alt="post"></img>}
+          <p className="text-sm px-4 py-2">{text}</p>
+          {imageUrl && <img src={imageUrl} alt="post"></img>}
           <div className="flex items-center justify-between px-2 pt-1">
             <span className="flex items-center gap-1">
               <AiFillLike className="text-blue-500" />
